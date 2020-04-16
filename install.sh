48aacd44544df4cad10fc056541e1558cd0ba92c
stoladev <mike@stoladev.com>
2020-04-15 22:41:47 -0500

    Updated installation script and added a cleaning script for post-installation alongside per-month cleaning.

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
-# ____ ___ ____ _    ____ ___  ____ _  _ 
-# [__   |  |  | |    |__| |  \ |___ |  | 
-# ___]  |  |__| |___ |  | |__/ |___  \/  
-#                                        
+# ____ ___ ____ _    ____ ___  ____ _  _
+# [__   |  |  | |    |__| |  \ |___ |  |
+# ___]  |  |__| |___ |  | |__/ |___  \/
+#
 
 # Installation protocol for post ArchBang
-# Does not include all recommended components for base Arch
-# Will make separate installer for base Arch in the future
+# ToDo: include all recommended components for base Arch
+# ToDo: make separate installer for base Arch in the future
 
-# DATA
+
 PKGS=(
 
-	# CRUCIAL				
+	# Crucial
 	'linux-lts'
 	'linux-lts-headers'
-	'linux-firmware'	
-	'hardinfo'								
-	'neofetch'								
-	'speedtest-cli'			
-	'libimobiledevice'				
+	'linux-firmware'
+	'hardinfo'
+	'speedtest-cli'
+	'libimobiledevice'
 
-	# AUDIO
+	# Audio
 	'alsa-plugins'
 	'pulseaudio'
 	'pulseaudio-alsa'
 	'pavucontrol'
 
-	# BLUETOOTH
+	# Bluetooth
+	# ToDo: Bluetooth isn't working for some reason. Probably due to overlapping programs. Fix on own system.
 	'bluez'
 	'bluez-utils'
 	'bluez-firmware'
@@ -33,7 +33,8 @@ PKGS=(
 	'pulseaudio-bluetooth'
 	'bluez-libs'
 
-	# DEVELOPMENT
+	# Development
+	# ToDo: Recheck programs that see actual use. Don't want bloatware for no reason. You don't even program in nodejs.
 	'avahi'
 	'clang'
 	'cmake'
@@ -41,98 +42,64 @@ PKGS=(
 	'glibc'
 	'nodejs'
 	'npm'
-	'zsh'									
-	'zsh-completions'	
-	'bash-completion'	
-	'cronie'								
-	'file-roller'		
-	'qtcreator'
-	'qt5-examples'
+	'zsh'
+	'zsh-completions'
+	'bash-completion'
+	'file-roller'
 	'filezilla'
 	'gimp'
-	'ristretto'
-	'virtualbox'
-	'virtualbox-host-modules-arch'
-	'playonlinux'
 
-	# SECURITY
-	# Wait a minute... why would I tell you what security packages I use?
-
-	# MEDIA
+	# Media
 	'vlc'
 	'obs-studio'
-	'youtube-dl'
-	
-	# EMAIL & CHAT
+
+	# Email and Chat
 	'thunderbird'
 	'discord'
 
-	# GAMES
-	'minecraft-launcher'
-	'steam'
-	'wine'
-	'proton'
-	'lutris'
-
-	# STYLE
+	# Style
+	# ToDo: Add more style components.
 	'slimlock'
 
 	)
 
 # START POINT
 echo "
-___  ____ ____ _ _  _ _  _ _ _  _ ____                 
-|__] |___ | __ | |\ | |\ | | |\ | | __                 
-|__] |___ |__] | | \| | \| | | \| |__]                                                                  
-_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _    
-| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |    
+___  ____ ____ _ _  _ _  _ _ _  _ ____
+|__] |___ | __ | |\ | |\ | | |\ | | __
+|__] |___ |__] | | \| | \| | | \| |__]
+_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
+| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
 | | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \| ...
-                                                       
+
 "
 
-sleep 5
-
-# YAY INSTALLATION
-cd "${HOME}"
+# YAY Installation
+cd "${HOME}" || exit
 git clone "https://aur.archlinux.org/yay.git"
-
-cd ${HOME}/yay
+cd "${HOME}"/yay || exit
 sudo pacman -S base-devel --noconfirm --needed
 makepkg -si --noconfirm --needed
 
-# WHATSAPP INSTALLATION
-cd "${HOME}"
-git clone "https://aur.archlinux.org/whatsapp-web-desktop"
-
-yay -S gconf nodejs-nativefier gtk2 --noconfirm --needed
-cd ${HOME}/whatsapp-web-desktop/
-makepkg -si --noconfirm --needed
-
-# SUBLIME INSTALLATION
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg
-sudo pacman-key --add sublimehq-pub.gpg
-sudo pacman-key --lsign-key 8A8F901A
-rm sublimehq-pub.gpg
-
-#PKG INSTALLATION
+# PKG Installation
 for PKG in "${PKGS[@]}"; do
-	yay -S --noconfirm $PKG
+	yay -S --noconfirm "$PKG"
 done
 
-# END POINT
+# End
 echo "
-_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _    
-| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |    
-| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|    
-____ ____ _  _ ___  _    ____ ___ ____                 
-|    |  | |\/| |__] |    |___  |  |___                 
+_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
+| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
+| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
+____ ____ _  _ ___  _    ____ ___ ____
+|    |  | |\/| |__] |    |___  |  |___
 |___ |__| |  | |    |___ |___  |  |___ .
 
-___  _    ____ ____ ____ ____                          
-|__] |    |___ |__| [__  |___                          
-|    |___ |___ |  | ___] |___                          
-____ ____ ___  ____ ____ ___                           
-|__/ |___ |__] |  | |  |  |                            
+___  _    ____ ____ ____ ____
+|__] |    |___ |__| [__  |___
+|    |___ |___ |  | ___] |___
+____ ____ ___  ____ ____ ___
+|__/ |___ |__] |  | |  |  |
 |  \ |___ |__] |__| |__|  |  .
 
 "
