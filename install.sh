3edcc9fa6cb85f6882f19d1cfca365a380414e71
stoladev <mike@stoladev.com>
2020-05-03 08:19:29 -0500

    Cleaned up and added TODO's

--- install.sh
+++ install.sh
@@ -1,78 +1,71 @@
-# ____ ___ ____ _    ____ ___  ____ _  _
-# [__   |  |  | |    |__| |  \ |___ |  |
-# ___]  |  |__| |___ |  | |__/ |___  \/
-#
-
-# Installation protocol for post ArchBang
-# ToDo: include all recommended components for base Arch
-# ToDo: make separate installer for base Arch in the future
-
-
 PKGS=(
 
-	# Crucial
+    # LTS Kernel
 	'linux-lts'
 	'linux-lts-headers'
 	'linux-firmware'
+
+
+    # Hardware and Internet
 	'hardinfo'
 	'speedtest-cli'
-	'libimobiledevice'
 
-	# Audio
+
+	# Audio and Video
 	'alsa-plugins'
 	'pulseaudio'
 	'pulseaudio-alsa'
 	'pavucontrol'
-
-	# Bluetooth
-	# ToDo: Bluetooth isn't working for some reason. Probably due to overlapping programs. Fix on own system.
-	'bluez'
-	'bluez-utils'
-	'bluez-firmware'
-	'blueberry'
-	'pulseaudio-bluetooth'
-	'bluez-libs'
-
-	# Development
-	# ToDo: Recheck programs that see actual use. Don't want bloatware for no reason. You don't even program in nodejs.
-	'avahi'
-	'clang'
-	'cmake'
-	'gcc'
-	'glibc'
-	'nodejs'
-	'npm'
-	'zsh'
-	'zsh-completions'
-	'bash-completion'
-	'file-roller'
-	'filezilla'
-	'gimp'
-
-	# Media
 	'vlc'
 	'obs-studio'
 
+
+	# Services/Hosts Discovery
+    'avahi'
+
+
+    # Compilers
+    'cmake'
+
+
+    # Nvim
+    'neovim'
+    'python-pynvim'
+
+    # FZF
+    'fzf'
+    'fd'
+    'ripgrep'
+    'the_silver_searcher'
+
+
+    # Bash
+    'bash-completion'
+	'file-roller'
+
+
+    # FTP
+	'filezilla'
+
+    
 	# Email and Chat
 	'thunderbird'
 	'discord'
 
-	# Style
-	# ToDo: Add more style components.
-	'slimlock'
+
+    # iPhone Mounting
+	# 'libimobiledevice'
 
 	)
 
-# START POINT
-echo "
-___  ____ ____ _ _  _ _  _ _ _  _ ____
-|__] |___ | __ | |\ | |\ | | |\ | | __
-|__] |___ |__] | | \| | \| | | \| |__]
-_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
-| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
-| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \| ...
 
-"
+# System Update 
+# TODO Update mirrorlist config to US 
+# TODO Add multilib through pacman.conf
+# TODO sudo pacman-key --init
+# TODO sudo pacman-key --populate archlinux
+# TODO sudo pacman -Syu
+
 
 # YAY Installation
 cd "${HOME}" || exit
@@ -81,25 +74,20 @@ cd "${HOME}"/yay || exit
 sudo pacman -S base-devel --noconfirm --needed
 makepkg -si --noconfirm --needed
 
+
 # PKG Installation
 for PKG in "${PKGS[@]}"; do
 	yay -S --noconfirm "$PKG"
 done
 
-# End
-echo "
-_ _  _ ____ ___ ____ _    _    ____ ___ _ ____ _  _
-| |\ | [__   |  |__| |    |    |__|  |  | |  | |\ |
-| | \| ___]  |  |  | |___ |___ |  |  |  | |__| | \|
-____ ____ _  _ ___  _    ____ ___ ____
-|    |  | |\/| |__] |    |___  |  |___
-|___ |__| |  | |    |___ |___  |  |___ .
 
-___  _    ____ ____ ____ ____
-|__] |    |___ |__| [__  |___
-|    |___ |___ |  | ___] |___
-____ ____ ___  ____ ____ ___
-|__/ |___ |__] |  | |  |  |
-|  \ |___ |__] |__| |__|  |  .
+# PKG Remover
+# TODO Add PKG_REMOVE, removing unnecessary packages
 
-"
+
+# Grub Bootloader Update
+# TODO Add sudo grub-mkconfig -o
+    # {LET USER CHOOSE /boot/ or /boot/efi/}/grub/grub.cfg
+
+
+
